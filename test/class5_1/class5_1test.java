f2d27d151b6106ae72d7231b9a8bab5c2c9934e7
g17926kc <g17926kc@gm.tsuda.ac.jp>
2019-05-17 17:57:24 +0900

    2回目

--- test/class5_1/class5_1test.java
+++ test/class5_1/class5_1test.java
@@ -1,5 +1,6 @@
 package class5_1;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -9,9 +10,16 @@ public class class5_1test {
 	@Test
 	public void translateEng1と入力するとoneが出力される() {
 		IntToEng ite = new IntToEng();
-		String input = "one";
-		String output = IntToEng.translateEng(1);
-		assertThat(output,is(input));
+		String expected = "one";
+		String actual = ite.translateEng(1);
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void translateEng21と入力するとtwentyoneが出力される() {
+		IntToEng ite = new IntToEng();
+		String expected = "twenty one";
+		String actual = ite.translateEng(21);
+		assertThat(actual,is(expected));
 	}
 
 }
